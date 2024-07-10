0fe9cebf1d04a029f3590489afc9428860cd6799
Thomas Labarussias <issif+github@gadz.org>
2024-07-10 15:11:08 +0200

    fix title
    Signed-off-by: Thomas Labarussias <issif+github@gadz.org>

--- main.go
+++ main.go
@@ -271,7 +271,7 @@ func updateIndexHtml(renderFolder string) {
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-	<title>Docker pull counts</title>
+	<title>Package pull counts</title>
 </head>
 <body>
 	<div class="row">
